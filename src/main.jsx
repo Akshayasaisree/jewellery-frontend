648e1c6ef5186fb46e88daa3b5558c940e857caf
Akshayasaisree <akshayasaisreegoli@gmail.com>
2026-03-06 15:32:36 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
-  <GoogleOAuthProvider clientId="564453147944-fco1mb1nkmvnnm1o9l5djc1d9s6grplh.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="178543182928-k4duvta253j8fuv8tudfvrdi2e6oog8t.apps.googleusercontent.com">
     <App />
     </GoogleOAuthProvider>
   </BrowserRouter>,
